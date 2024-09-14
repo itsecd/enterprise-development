4a6f94c526ffd0fb8f883f5b6e1aa1ca0b20804c
Acebiscuits <27roter72@gmail.com>
2024-09-14 15:33:32 +0400

    add: classes structure

---
+++ lab1/Media.Domain/Artist.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Media.Domain
+{
+    internal class Artist
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public List<Album> Albums { get; set; } = new List<Album>();
+        public List<Genre> Genres { get; set; } = new List<Genre> { };
+    }
+}
