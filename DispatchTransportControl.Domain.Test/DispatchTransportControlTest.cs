6e845b6802e9224f1653210b12ef28caecd6e895
Donistr <deniselag@mail.ru>
2024-09-20 13:28:40 +0400

    test: сделал тесты в соответствии с запросами, представленными в задании

---
+++ DispatchTransportControl.Domain.Test/DispatchTransportControlTest.cs
@@ -0,0 +1,134 @@
+﻿namespace DispatchTransportControl.Domain.Test;
+
+public class DispatchTransportControlTest(TestDataProvider testDataProvider) : IClassFixture<TestDataProvider>
+{
+    [Fact]
+    public void GetInfoAboutVehicleById()
+    {
+        int vehicleId = 1;
+        Vehicle? vehicle = testDataProvider.Vehicles.FirstOrDefault(v => v.Id == vehicleId);
+
+        Assert.NotNull(vehicle);
+        Assert.Equal(vehicleId, vehicle.Id);
+        Assert.Equal("registrationNumber1", vehicle.RegistrationNumber);
+        Assert.Equal(VehicleType.Bus, vehicle.VehicleType);
+        Assert.Equal(testDataProvider.VehicleModels[0], vehicle.VehicleModel);
+        Assert.Equal(1993, vehicle.YearOfManufacture);
+    }
+
+    [Fact]
+    public void GetDriversByPeriod()
+    {
+        DateTime startDate = testDataProvider.DatetimeNow.AddHours(-5);
+        DateTime endDate = testDataProvider.DatetimeNow.AddHours(4);
+
+        List<Driver> drivers = testDataProvider.RouteAssignments
+            .Where(ra => startDate <= ra.StartTime && ra.EndTime <= endDate)
+            .Select(ra => ra.Driver)
+            .Distinct()
+            .OrderBy(d => d.Surname).ThenBy(d => d.Name).ThenBy(d => d.Patronymic)
+            .ToList();
+
+        Assert.Equal(4, drivers.Count);
+        Assert.Equal("driverSurname1", testDataProvider.Drivers[0].Surname);
+        Assert.Equal("driverSurname2", testDataProvider.Drivers[1].Surname);
+        Assert.Equal("driverSurname3", testDataProvider.Drivers[2].Surname);
+        Assert.Equal("driverSurname4", testDataProvider.Drivers[3].Surname);
+    }
+
+    [Fact]
+    public void GetTotalTripTimeByVehicleType()
+    {
+        Dictionary<VehicleType, double> tripDurationsForVehicleType = testDataProvider.RouteAssignments
+            .GroupBy(ra => new { ra.Vehicle.VehicleType })
+            .ToDictionary
+            (
+                group => group.Key.VehicleType,
+                group => group.Sum(ra => (ra.EndTime - ra.StartTime).TotalHours)
+            );
+
+        Assert.Equal(6, tripDurationsForVehicleType[VehicleType.Bus]);
+        Assert.Equal(1, tripDurationsForVehicleType[VehicleType.Trolley]);
+        Assert.Equal(2, tripDurationsForVehicleType[VehicleType.Tram]);
+    }
+
+    [Fact]
+    public void GetTotalTripTimeByVehicleModel()
+    {
+        Dictionary<int, double> tripDurationsForVehicleModel = testDataProvider.RouteAssignments
+            .GroupBy(ra => new { ra.Vehicle.VehicleModel.Id })
+            .ToDictionary
+            (
+                group => group.Key.Id,
+                group => group.Sum(ra => (ra.EndTime - ra.StartTime).TotalHours)
+            );
+
+        Assert.Equal(1, tripDurationsForVehicleModel[1]);
+        Assert.Equal(1, tripDurationsForVehicleModel[2]);
+        Assert.Equal(2, tripDurationsForVehicleModel[3]);
+        Assert.Equal(3, tripDurationsForVehicleModel[4]);
+        Assert.Equal(2, tripDurationsForVehicleModel[5]);
+    }
+
+    [Fact]
+    public void GetTop5DriversByTripCount()
+    {
+        var topDrivers = testDataProvider.RouteAssignments
+            .GroupBy(ra => ra.Driver)
+            .Select(group => new
+            {
+                Driver = group.Key,
+                TripCount = group.Count()
+            })
+            .OrderByDescending(g => g.TripCount)
+            .Take(5)
+            .ToList();
+
+        Assert.Equal(5, topDrivers.Count);
+        Assert.Equal(testDataProvider.Drivers[3], topDrivers[0].Driver);
+    }
+
+    [Fact]
+    public void GetDriverTripStats()
+    {
+        var driverStats = testDataProvider.RouteAssignments
+            .GroupBy(ra => ra.Driver)
+            .ToDictionary(
+                group => group.Key,
+                group => new
+                {
+                    TripCount = group.Count(),
+                    AvgTime = group.Average(ra => (ra.EndTime - ra.StartTime).TotalHours),
+                    MaxTime = group.Max(ra => (ra.EndTime - ra.StartTime).TotalHours)
+                }
+            );
+
+        Assert.Equal(5, driverStats.Count);
+        Assert.Equal(2, driverStats[testDataProvider.Drivers[3]].TripCount);
+        Assert.Equal(1.5, driverStats[testDataProvider.Drivers[3]].AvgTime);
+        Assert.Equal(2, driverStats[testDataProvider.Drivers[3]].MaxTime);
+    }
+
+    [Fact]
+    public void GetVehiclesWithMaxTripsForPeriod()
+    {
+        DateTime startDate = testDataProvider.DatetimeNow.AddHours(-5);
+        DateTime endDate = testDataProvider.DatetimeNow.AddHours(10);
+
+        int maxTripCount = testDataProvider.RouteAssignments.Where(ra => ra.StartTime >= startDate && ra.EndTime <= endDate)
+            .GroupBy(ra => ra.Vehicle)
+            .Select(group => group.Count())
+            .Max();
+
+        var vehiclesWithMaxTrips = testDataProvider.RouteAssignments.Where(ra => ra.StartTime >= startDate && ra.EndTime <= endDate)
+            .GroupBy(ra => ra.Vehicle)
+            .Select(group => new { Vehicle = group.Key, TripCount = group.Count() })
+            .Where(g => g.TripCount == maxTripCount)
+            .ToList();
+
+        Assert.Single(vehiclesWithMaxTrips);
+        Assert.Equal(testDataProvider.Vehicles[4], vehiclesWithMaxTrips[0].Vehicle);
+        Assert.Equal(2, vehiclesWithMaxTrips[0].TripCount);
+    }
+
+}
---
+++ DispatchTransportControl.Domain.Test/TestDataProvider.cs
@@ -0,0 +1,183 @@
+﻿namespace DispatchTransportControl.Domain.Test;
+
+public class TestDataProvider
+{
+    public DateTime DatetimeNow = DateTime.Now;
+
+    public List<VehicleModel> VehicleModels =
+    [
+        new() { Id = 1, Name = "vehicleName1", LowFloor = true, MaxCapacity = 10 },
+        new() { Id = 2, Name = "vehicleName2", LowFloor = false, MaxCapacity = 20 },
+        new() { Id = 3, Name = "vehicleName3", LowFloor = false, MaxCapacity = 30 },
+        new() { Id = 4, Name = "vehicleName4", LowFloor = true, MaxCapacity = 12 },
+        new() { Id = 5, Name = "vehicleName5", LowFloor = true, MaxCapacity = 15 }
+    ];
+
+    public List<Driver> Drivers =
+    [
+        new()
+        {
+            Id = 1,
+            Name = "driverName1",
+            Surname = "driverSurname1",
+            Patronymic = "driverPatronymic1",
+            Passport = "passport1",
+            DriverLicense = "driverLicense1",
+            Address = "address1",
+            Phone = "phone1"
+        },
+        new()
+        {
+            Id = 2,
+            Name = "driverName2",
+            Surname = "driverSurname2",
+            Patronymic = "driverPatronymic2",
+            Passport = "passport2",
+            DriverLicense = "driverLicense2",
+            Address = "address2",
+            Phone = "phone2"
+        },
+        new()
+        {
+            Id = 3,
+            Name = "driverName3",
+            Surname = "driverSurname3",
+            Patronymic = "driverPatronymic3",
+            Passport = "passport3",
+            DriverLicense = "driverLicense3",
+            Address = "address3",
+            Phone = "phone3"
+        },
+        new()
+        {
+            Id = 4,
+            Name = "driverName4",
+            Surname = "driverSurname4",
+            Patronymic = "driverPatronymic4",
+            Passport = "passport4",
+            DriverLicense = "driverLicense4",
+            Address = "address4",
+            Phone = "phone4"
+        },
+        new()
+        {
+            Id = 5,
+            Name = "driverName5",
+            Surname = "driverSurname5",
+            Patronymic = "driverPatronymic5",
+            Passport = "passport5",
+            DriverLicense = "driverLicense5",
+            Address = "address5",
+            Phone = "phone5"
+        }
+    ];
+
+    public List<Vehicle> Vehicles;
+
+    public List<RouteAssignment> RouteAssignments;
+
+    public TestDataProvider()
+    {
+        Vehicles =
+        [
+            new Vehicle
+            {
+                Id = 1,
+                RegistrationNumber = "registrationNumber1",
+                VehicleType = VehicleType.Bus,
+                VehicleModel = VehicleModels[0],
+                YearOfManufacture = 1993
+            },
+            new Vehicle
+            {
+                Id = 2,
+                RegistrationNumber = "registrationNumber2",
+                VehicleType = VehicleType.Bus,
+                VehicleModel = VehicleModels[2],
+                YearOfManufacture = 1999
+            },
+            new Vehicle
+            {
+                Id = 3,
+                RegistrationNumber = "registrationNumber3",
+                VehicleType = VehicleType.Trolley,
+                VehicleModel = VehicleModels[1],
+                YearOfManufacture = 2005
+            },
+            new Vehicle
+            {
+                Id = 4,
+                RegistrationNumber = "registrationNumber4",
+                VehicleType = VehicleType.Tram,
+                VehicleModel = VehicleModels[4],
+                YearOfManufacture = 2007
+            },
+            new Vehicle
+            {
+                Id = 5,
+                RegistrationNumber = "registrationNumber5",
+                VehicleType = VehicleType.Bus,
+                VehicleModel = VehicleModels[3],
+                YearOfManufacture = 1823
+            }
+        ];
+
+        RouteAssignments =
+        [
+            new RouteAssignment
+            {
+                Id = 1,
+                Vehicle = Vehicles[0],
+                Driver = Drivers[0],
+                RouteNumber = "routeNumber1",
+                StartTime = DatetimeNow,
+                EndTime = DatetimeNow.AddHours(1)
+            },
+            new RouteAssignment
+            {
+                Id = 2,
+                Vehicle = Vehicles[1],
+                Driver = Drivers[2],
+                RouteNumber = "routeNumber2",
+                StartTime = DatetimeNow.AddHours(1),
+                EndTime = DatetimeNow.AddHours(3)
+            },
+            new RouteAssignment
+            {
+                Id = 3,
+                Vehicle = Vehicles[2],
+                Driver = Drivers[1],
+                RouteNumber = "routeNumber3",
+                StartTime = DatetimeNow.AddHours(1),
+                EndTime = DatetimeNow.AddHours(2)
+            },
+            new RouteAssignment
+            {
+                Id = 4,
+                Vehicle = Vehicles[3],
+                Driver = Drivers[4],
+                RouteNumber = "routeNumber4",
+                StartTime = DatetimeNow.AddHours(3),
+                EndTime = DatetimeNow.AddHours(5)
+            },
+            new RouteAssignment
+            {
+                Id = 5,
+                Vehicle = Vehicles[4],
+                Driver = Drivers[3],
+                RouteNumber = "routeNumber5",
+                StartTime = DatetimeNow.AddHours(3),
+                EndTime = DatetimeNow.AddHours(4)
+            },
+            new RouteAssignment
+            {
+                Id = 6,
+                Vehicle = Vehicles[4],
+                Driver = Drivers[3],
+                RouteNumber = "routeNumber6",
+                StartTime = DatetimeNow.AddHours(7),
+                EndTime = DatetimeNow.AddHours(9)
+            }
+        ];
+    }
+}
