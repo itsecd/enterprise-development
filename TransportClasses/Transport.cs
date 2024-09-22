77f07781c4180d9c9aba943c50ab9a9c6dff48ce
Dmitri <gorsh03@gmail.com>
2024-09-22 21:27:59 +0400

    Create Classes

---
+++ TransportClasses/Transport.cs
@@ -0,0 +1,53 @@
+﻿namespace TransportClass
+{
+    /// <summary>
+    /// Транспортное средство
+    /// </summary>
+    public class Transport
+    {
+        /// <summary>
+        /// Список видов транспорта
+        /// </summary>
+        public enum VehicleType
+        {
+            Bus,
+            Tram,
+            Trolleybus
+        }
+
+        /// <summary>
+        /// Индентификатор транспорта
+        /// </summary>
+        public int ID { get; set; }
+
+        /// <summary>
+        /// Номер автомобиля
+        /// </summary>
+        public string LicensePlate { get; set; }
+
+        /// <summary>
+        /// Вид транспорта
+        /// </summary>
+        public VehicleType Type { get; set; }
+
+        /// <summary>
+        /// Название модели транспорта
+        /// </summary>
+        public string ModelName { get; set; }
+
+        /// <summary>
+        /// Тип транспорта (низкопольный / ненизкопльный) 
+        /// </summary>
+        public bool? IsLowFloor { get; set; }
+
+        /// <summary>
+        /// Максимальная вместительность
+        /// </summary>
+        public int? MaxCapacity { get; set; }
+
+        /// <summary>
+        /// Год выпуска транспорта
+        /// </summary>
+        public int? YearOfManufacture { get; set; }
+    }
+}
