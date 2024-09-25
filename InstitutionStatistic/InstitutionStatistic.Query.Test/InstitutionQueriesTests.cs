a92aedf901f6bcebf21cf3f3b093a7274349ef11
Леонид Пахомов <pahomov@corp.ir-tech.ru>
2024-09-25 19:01:03 +0400

    Переделал тесты для unit. Теперь заработают акшоны

--- InstitutionStatistic/InstitutionStatistic.Query.Test/InstitutionQueriesTests.cs
+++ InstitutionStatistic/InstitutionStatistic.Query.Test/InstitutionQueriesTests.cs
@@ -6,100 +6,104 @@ namespace InstitutionStatistic.Query.Test;
 /// <summary>
 /// тесты для проверки запросов об институтах
 /// </summary>
-[TestFixture]
 public class InstitutionQueriesTests : TestBase
 {
     private InstitutinQuery _institutinQuery;
 
-    [SetUp]
-    public void SetUp()
+    public InstitutionQueriesTests()
     {
         _institutinQuery = new InstitutinQuery(Institutions);
     }
 
     #region Вывести информацию о выбранном вузе
-    [Test]
+    [Fact]
     public void GetInstitutionInfoTest()
     {
         var ssau = "СГАУ";
         var pguty = "ПГУТИ";
         var notExisted = "notExisted";
 
-        Assert.That(_institutinQuery.GetByName(ssau), Is.EqualTo(Institutions[0]));
-        Assert.That(_institutinQuery.GetByName(pguty), Is.EqualTo(Institutions[2]));
-        Assert.That(_institutinQuery.GetByName(notExisted), Is.EqualTo(null));
+        Assert.Equal(_institutinQuery.GetByName(ssau), Institutions[0]);
+        Assert.Equal(_institutinQuery.GetByName(pguty), Institutions[2]);
+        Assert.Equal(_institutinQuery.GetByName(notExisted), null);
     }
     #endregion
 
     #region Вывести информацию о факультетах, кафедрах и специальностях данного вуза
-    [TestCase("СГАУ", new string[] { "FC1", "FC2" })]
-    [TestCase("ПГУТИ", new string[] { "FC4" })]
+    [Theory]
+    [InlineData("СГАУ", new[] { "FC1", "FC2" })]
+    [InlineData("ПГУТИ", new[] { "FC4" })]
     public void GetInstitutionFacultiesTest(string name, string[] expected)
     {
-        Assert.That(
+        Assert.Equal(
             _institutinQuery.GetInstitutionFaculties(x => x.Name, name).Select(x => x.Name).ToList(),
-            Is.EqualTo(expected));
+            expected);
     }
 
-    [TestCase("СГАУ", new string[] { "SPEC1", "SPEC2", "SPEC3" })]
-    [TestCase("ПГУТИ", new string[] { "SPEC1", "SPEC2", "SPEC3", "SPEC4" })]
+    [Theory]
+    [InlineData("СГАУ", new string[] { "SPEC1", "SPEC2", "SPEC3" })]
+    [InlineData("ПГУТИ", new string[] { "SPEC1", "SPEC2", "SPEC3", "SPEC4" })]
     public void GetInstitutionSpecialitiesTest(string name, string[] expected)
     {
-        Assert.That(
+        Assert.Equal(
             _institutinQuery.GetInstitutionSpecialities(x => x.Name, name).Select(x => x.Name).ToList(),
-            Is.EqualTo(expected));
+            expected);
     }
 
-    [TestCase("СГАУ", new string[] { "ГИИБ", "ИСТ" })]
-    [TestCase("ПГУТИ", new string[] { "TEST1", "TEST2" })]
+    [Theory]
+    [InlineData("СГАУ", new string[] { "ГИИБ", "ИСТ" })]
+    [InlineData("ПГУТИ", new string[] { "TEST1", "TEST2" })]
     public void GetInstitutionDepartmentsTest(string name, string[] expected)
     {
-        Assert.That(
+        Assert.Equal(
             _institutinQuery.GetInstitutionDepartments(x => x.Name, name).Select(x => x.Name).ToList(),
-            Is.EqualTo(expected));
+            expected);
     }
     #endregion
 
     #region Вывести информацию о ВУЗах с максимальным количеством кафедр, упорядочить по названию
-    [Test]
+    [Fact]
     public void GetMaxDepartmentInstitutionsTest()
     {
-        Assert.That(
+        Assert.Equal(
             _institutinQuery.GetMaxDepartmentInstitutions().Select(x => x.Name).ToList(),
-            Is.EqualTo(new List<string> { "ПГУТИ", "СГАУ" }));
+            new List<string> { "ПГУТИ", "СГАУ" });
     }
     #endregion
 
     #region Вывести информацию о ВУЗах с заданной собственностью учреждения, и количество групп в ВУЗе
-    [Test]
+    [Fact]
     public void GetInstitutionsTest()
     {
-        Assert.That(
+        Assert.Equal(
             _institutinQuery.GetInstitutions(InstitutionOwnership.Municipality, 5).Select(x => x.Name).ToList(),
-            Is.EqualTo(new List<string> { "САМГТУ"}));
+            new List<string> { "САМГТУ"});
     }
     #endregion
 
     #region  Вывести информацию о количестве факультетов, кафедр, специальностей по каждому типу собственности учреждения и собственности здания
-    [TestCase(InstitutionOwnership.Municipality, BuildingOwnership.Municipality, ExpectedResult = 1)]
-    [TestCase(InstitutionOwnership.Municipality, BuildingOwnership.Federal, ExpectedResult = 2)]
-    public int GetFacultiesCountByOwnershipTest(InstitutionOwnership institutionOwnership, BuildingOwnership buildingOwnership)
+    [Theory]
+    [InlineData(InstitutionOwnership.Municipality, BuildingOwnership.Municipality, 1)]
+    [InlineData(InstitutionOwnership.Municipality, BuildingOwnership.Federal, 2)]
+    public void GetFacultiesCountByOwnershipTest(InstitutionOwnership institutionOwnership, BuildingOwnership buildingOwnership, int expectedResult)
     {
-        return _institutinQuery.GetFacultiesCountByOwnership(institutionOwnership, buildingOwnership);
+        Assert.Equal(_institutinQuery.GetFacultiesCountByOwnership(institutionOwnership, buildingOwnership), expectedResult);
     }
 
-    [TestCase(InstitutionOwnership.Personal, BuildingOwnership.Personal, ExpectedResult = 2)]
-    [TestCase(InstitutionOwnership.Personal, BuildingOwnership.Federal, ExpectedResult = 1)]
-    public int GetDepartmentsCountByOwnershipTest(InstitutionOwnership institutionOwnership, BuildingOwnership buildingOwnership)
+    [Theory]
+    [InlineData(InstitutionOwnership.Personal, BuildingOwnership.Personal, 2)]
+    [InlineData(InstitutionOwnership.Personal, BuildingOwnership.Federal, 1)]
+    public void GetDepartmentsCountByOwnershipTest(InstitutionOwnership institutionOwnership, BuildingOwnership buildingOwnership, int expectedResult)
     {
-        return _institutinQuery.GetDepartmentsCountByOwnership(institutionOwnership, buildingOwnership);
+        Assert.Equal(_institutinQuery.GetDepartmentsCountByOwnership(institutionOwnership, buildingOwnership), expectedResult);
     }
 
-    [TestCase(InstitutionOwnership.Municipality, BuildingOwnership.Federal, ExpectedResult = 3)]
-    [TestCase(InstitutionOwnership.Municipality, BuildingOwnership.Municipality, ExpectedResult = 4)]
-    public int GetSpecialitiesCountByOwnershipTest(InstitutionOwnership institutionOwnership, BuildingOwnership buildingOwnership)
+    [Theory]
+    [InlineData(InstitutionOwnership.Municipality, BuildingOwnership.Federal, 3)]
+    [InlineData(InstitutionOwnership.Municipality, BuildingOwnership.Municipality, 4)]
+    public void GetSpecialitiesCountByOwnershipTest(InstitutionOwnership institutionOwnership, BuildingOwnership buildingOwnership, int expectedResult)
     {
-        return _institutinQuery.GetSpecialitiesCountByOwnership(institutionOwnership, buildingOwnership);
+        Assert.Equal(_institutinQuery.GetSpecialitiesCountByOwnership(institutionOwnership, buildingOwnership), expectedResult);
     }
     #endregion
 }
--- InstitutionStatistic/InstitutionStatistic.Query.Test/SpecialityInfoTests.cs
+++ InstitutionStatistic/InstitutionStatistic.Query.Test/SpecialityInfoTests.cs
@@ -5,25 +5,24 @@ namespace InstitutionStatistic.Query.Test;
 /// <summary>
 /// тесты для проверки запросов о специальностях
 /// </summary>
-[TestFixture]
+
 public class SpecialityInfoTests: TestBase
 {
     private SpecialityQuery _specialitiesQuery;
 
-    [SetUp]
-    public void SetUp()
+    public SpecialityInfoTests()
     {
         _specialitiesQuery = new SpecialityQuery(Specialities);
     }
 
 
     #region Вывести информацию о топ 5 популярных специальностях
-    [Test]
-    public void Test1()
+    [Fact]
+    public void GetTopFiveSpecialitiesTest()
     {
-        Assert.That(
+        Assert.Equal(
             _specialitiesQuery.GetTopFiveSpecialities().Select(x => x.Name).ToList(),
-            Is.EquivalentTo(new List<string> { "SPEC1", "SPEC2", "SPEC3", "SPEC4", "SPEC5" }));
+            ["SPEC1", "SPEC2", "SPEC3", "SPEC4", "SPEC5"]);
     }
     #endregion
 }
