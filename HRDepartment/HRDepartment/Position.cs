dc6ffa928b3d20f2c853e7445544786ca722d961
PolinaKrp <karpacevapolina@gmail.com>
2024-09-30 23:18:33 +0400

    Внесла изменения в классы

---
+++ HRDepartment/HRDepartment/Position.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRDepartment.Domain;
+
+/// <summary>
+/// Класс должность в компании
+/// </summary>
+public class Position
+{
+    /// <summary>
+    /// Уникальный идентификатор должности
+    /// </summary>
+    public required int Id { get; set; }
+
+    /// <summary>
+    /// Название должности
+    /// </summary>
+    public required string Name { get; set; }
+
+    /// <summary>
+    /// Список сотрудников, занимающих эту должность
+    /// </summary>
+    public List<Employee> Employees { get; set; } = new List<Employee>();
+}
