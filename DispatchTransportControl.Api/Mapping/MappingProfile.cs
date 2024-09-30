4e7618b9d7afd0665adade2b03eabe0c4fbcd1fb
Donistr <deniselag@mail.ru>
2024-09-30 17:25:14 +0400

    feat: добавил настройки для маппинга энтити в дто и наоборот

---
+++ DispatchTransportControl.Api/Mapping/MappingProfile.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using DispatchTransportControl.Api.DTO;
+using DispatchTransportControl.Domain;
+
+namespace DispatchTransportControl.Api.Mapping;
+
+/// <summary>
+/// Класс предназначен для настройки маппинга энтити в дто
+/// </summary>
+public class MappingProfile : Profile
+{
+    public MappingProfile()
+    {
+        CreateMap<Driver, DriverDto>();
+        CreateMap<DriverDto, Driver>();
+        CreateMap<Driver, DriverCreateDto>();
+        CreateMap<DriverCreateDto, Driver>();
+
+        CreateMap<VehicleModel, VehicleModelDto>();
+        CreateMap<VehicleModelDto, VehicleModel>();
+        CreateMap<VehicleModel, VehicleModelCreateDto>();
+        CreateMap<VehicleModelCreateDto, VehicleModel>();
+
+        CreateMap<Vehicle, VehicleDto>();
+        CreateMap<VehicleDto, Vehicle>();
+
+        CreateMap<RouteAssignment, RouteAssignmentDto>();
+        CreateMap<RouteAssignmentDto, RouteAssignment>();
+    }
+}
