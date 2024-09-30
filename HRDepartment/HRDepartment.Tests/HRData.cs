0b419a893ec74a023ee7358761ce42d61d515be2
PolinaKrp <karpacevapolina@gmail.com>
2024-09-30 23:42:14 +0400

    Попытка исправить кодировку

---
+++ HRDepartment/HRDepartment.Tests/HRData.cs
@@ -0,0 +1,40 @@
+﻿#pragma encoding utf-8
+using HRDepartment.Domain;
+
+namespace HRDepartment.Tests;
+
+/// <summary>
+/// �����, ������� ������������ ������ ��� ������������ 
+/// </summary>
+public class HRData
+{
+    /// <summary>
+    /// ������ �����������
+    /// </summary>
+    public List<Employee>? Employees { get; set; }
+
+    /// <summary>
+    /// ������ �������
+    /// </summary>
+    public List<Department>? Departments { get; set; }
+
+    /// <summary>
+    /// ������ ����������
+    /// </summary>
+    public List<Position>? Positions { get; set; }
+
+    /// <summary>
+    /// ������ �����
+    /// </summary>
+    public List<Benefit>? Benefits { get; set; }
+
+    /// <summary>
+    /// ������ ������� � �������� ������������
+    /// </summary>
+    public List<WorkHistory>? WorkHistories { get; set; }
+
+    /// <summary>
+    /// ������ ����������� �����������
+    /// </summary>
+    public List<Union>? Unions { get; set; }
+}
