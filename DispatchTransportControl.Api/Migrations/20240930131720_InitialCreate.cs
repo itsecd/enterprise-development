7b7f55a151d78554efe7c00004d83c0c1cc5f5d8
Donistr <deniselag@mail.ru>
2024-09-30 17:18:00 +0400

    feat: сгенерировал миграции для создания таблиц в бд

---
+++ DispatchTransportControl.Api/Migrations/20240930131720_InitialCreate.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace DispatchTransportControl.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "driver",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    name = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    surname = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    patronymic = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    passport = table.Column<string>(type: "character varying(20)", maxLength: 20, nullable: false),
+                    driver_license = table.Column<string>(type: "character varying(20)", maxLength: 20, nullable: false),
+                    address = table.Column<string>(type: "character varying(256)", maxLength: 256, nullable: false),
+                    phone = table.Column<string>(type: "character varying(20)", maxLength: 20, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_driver", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "vehicle_model",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    name = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    low_floor = table.Column<bool>(type: "boolean", nullable: false),
+                    max_capacity = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_vehicle_model", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "vehicle",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    registration_number = table.Column<string>(type: "character varying(20)", maxLength: 20, nullable: false),
+                    vehicle_type = table.Column<int>(type: "integer", nullable: false),
+                    VehicleModelId = table.Column<int>(type: "integer", nullable: false),
+                    year_of_manufacture = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_vehicle", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_vehicle_vehicle_model_VehicleModelId",
+                        column: x => x.VehicleModelId,
+                        principalTable: "vehicle_model",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "route_assignment",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    VehicleId = table.Column<int>(type: "integer", nullable: false),
+                    DriverId = table.Column<int>(type: "integer", nullable: false),
+                    route_number = table.Column<string>(type: "character varying(20)", maxLength: 20, nullable: false),
+                    start_time = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    end_time = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_route_assignment", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_route_assignment_driver_DriverId",
+                        column: x => x.DriverId,
+                        principalTable: "driver",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_route_assignment_vehicle_VehicleId",
+                        column: x => x.VehicleId,
+                        principalTable: "vehicle",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_route_assignment_DriverId",
+                table: "route_assignment",
+                column: "DriverId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_route_assignment_VehicleId",
+                table: "route_assignment",
+                column: "VehicleId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_vehicle_VehicleModelId",
+                table: "vehicle",
+                column: "VehicleModelId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "route_assignment");
+
+            migrationBuilder.DropTable(
+                name: "driver");
+
+            migrationBuilder.DropTable(
+                name: "vehicle");
+
+            migrationBuilder.DropTable(
+                name: "vehicle_model");
+        }
+    }
+}
