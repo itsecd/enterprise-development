a13de25cd2c8e59846c36b239d04e7eb6beca99e
Степан Семыкин <semykinstepan2@gmail.com>
2024-10-04 17:52:31 +0400

    Добавлены documentation comments

--- TaxiCompany.Domain/TaxiCompany.Domain/Client.cs
+++ TaxiCompany.Domain/TaxiCompany.Domain/Client.cs
@@ -1,10 +1,13 @@
 ﻿namespace TaxiCompany.Domain;
-
+/// <summary>
+/// Класс <c>Клиент</c> хранит информацию о клиенте
+/// </summary>
 public class Client
 {
+    ///<value>Идентификатор клиента</value>
     public required int Id { get; set; }
-
+    ///<value>Имя и фамилия</value>
     public required string FullName { get; set; }
-
+    ///<value>Номер телефона</value>
     public required string PhoneNumber { get; set; }
 }
--- TaxiCompany.Domain/TaxiCompany.Domain/Trip.cs
+++ TaxiCompany.Domain/TaxiCompany.Domain/Trip.cs
@@ -1,20 +1,23 @@
 ﻿namespace TaxiCompany.Domain;
-
+/// <summary>
+/// Класс <c>Поездка</c> хранит информацию о поездке
+/// </summary>
 public class Trip
 {
+    /// <value>Идентификатор поездки</value>
     public required int Id { get; set; }
-
+    /// <value>Пункт отправления</value>
     public required string Departure { get; set; }
-
+    /// <value>Пункт назначения</value>
     public required string Destination { get; set; }
-
+    /// <value>Дата поездки</value>
     public required DateTime Date { get; set; }
-
+    /// <value>Время в движении</value>
     public TimeOnly DrivingTime { get; set; }
-
+    /// <value>Стоимость</value>
     public required decimal Cost { get; set; }
-
+    /// <value>Идентификатор клиента</value>
     public required int AssignedClientId { get; set; }
-
+    /// <value>Идентификатор автомобиля</value>
     public required int AssignedCarId { get; set; }
 }
