61e7df3cbeac02716ff5e6ef39254d51f0b58ccf
Gironape <andreyka_kazakov_03@mail.ru>
2024-10-06 17:25:43 +0400

    Create Supply
    Create Supply

---
+++ WarehouseEnterprice/WarehouseEnterpriseApp/Supply.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WarehouseEnterpriseApp
+{
+    /// <summary>
+    /// Поставка товара
+    /// </summary>
+    public class Supply
+    {
+        /// <summary>
+        /// Идентификатор
+        /// </summary>
+        public required int Id { get; set; }
+        /// <summary>
+        /// Товар
+        /// </summary>
+        public required Product Product { get; set; }
+        /// <summary>
+        /// Организация
+        /// </summary>
+        public required Organization Organization { get; set; }
+        /// <summary>
+        /// Дата поставки
+        /// </summary>
+        public required DateTime SupplyDate { get; set; }
+        /// <summary>
+        /// Количество поставленного товара
+        /// </summary>
+        public required int Quantity { get; set; }
+    }
+}
