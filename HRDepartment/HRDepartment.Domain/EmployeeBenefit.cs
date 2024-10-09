f5050fc326ad929e27c3c3bc087c14d17588af5f
PolinaKrp <karpacevapolina@gmail.com>
2024-10-09 23:08:20 +0400

    Переделала все классы по ER диаграмме

---
+++ HRDepartment/HRDepartment.Domain/EmployeeBenefit.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRDepartment.Domain;
+
+/// <summary>
+/// Промежуточный класс, представляющий льготы, которые получает сотрудник
+/// </summary>
+public class EmployeeBenefit
+{
+    /// <summary>
+    /// Уникальный идентификатор записи о льготе сотрудника
+    /// </summary>
+    public required int Id { get; set; }
+
+    /// <summary>
+    /// Идентификатор сотрудника, который получил льготу
+    /// </summary>
+    public required int EmployeeId { get; set; }
+
+    /// <summary>
+    /// Объект сотрудника, который получил льготу
+    /// </summary>
+    public required Employee Employee { get; set; }
+
+    /// <summary>
+    /// Идентификатор типа льготы
+    /// </summary>
+    public required int BenefitTypeId { get; set; }
+
+    /// <summary>
+    /// Объект типа льготы, которую получил сотрудник
+    /// </summary>
+    public required BenefitType BenefitType { get; set; }
+
+    /// <summary>
+    /// Дата выдачи льготы
+    /// </summary>
+    public required DateTime IssueDate { get; set; }
+}
---
+++ HRDepartment/HRDepartment.Domain/Position.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRDepartment.Domain;
+
+/// <summary>
+/// Класс, представляющий должность на предприятии
+/// </summary>
+public class Position
+{
+    /// <summary>
+    /// Уникальный идентификатор должности
+    /// </summary>
+    public required int Id { get; set; }
+
+    /// <summary>
+    /// Название должности
+    /// </summary>
+    public required string Name { get; set; }
+
+    /// <summary>
+    /// Идентификатор отдела, к которому относится должность
+    /// </summary>
+    public required int DepartmentId { get; set; }
+
+    /// <summary>
+    /// Объект отдела, к которому относится должность (связь с Department)
+    /// </summary>
+    public required Department Department { get; set; }
+
+    /// <summary>
+    /// Коллекция сотрудников, занимающих эту должность 
+    /// </summary>
+    public List<EmployeePosition> EmployeePositions { get; set; } = new List<EmployeePosition>();
+}
