5dccb37f4931f282869f57eeffceab1ce67b0e5a
Vlad Barishnikov <barushnikovvs@gmail.com>
2024-10-10 21:47:59 +0400

    refactor: изменил тесты в связи с изменением модели

--- DistrictEnterpriseStatisticalData.Domain.Test/DataProvider.cs
+++ DistrictEnterpriseStatisticalData.Domain.Test/DataProvider.cs
@@ -1,183 +1,265 @@
-﻿namespace DistrictEnterpriseStatisticalData.Domain.Test;
+﻿using DistrictEnterpriseStatisticalData.Domain.Entity;
+
+namespace DistrictEnterpriseStatisticalData.Domain.Test;
 
 public class DataProvider
 {
+    public List<EnterpriseType> EnterpriseTypes =
+    [
+        new() { Type = "Agriculture" },
+        new() { Type = "Transport" },
+        new() { Type = "LightIndustry" },
+        new() { Type = "HeavyIndustry" },
+        new() { Type = "Building" },
+        new() { Type = "MaterialAndTechnicalSupply" },
+    ];
+
+    public List<FormOfOwnership> FormsOfOwnership =
+    [
+        new() { Form = "StateFederal" },
+        new() { Form = "MunicipalUrban" },
+        new() { Form = "TOO" },
+        new() { Form = "Private" },
+        new() { Form = "JointStock" },
+    ];
+
     public List<Supply> Supplies =
     [
-        new(){ SupplyId = 1, Quantity = 1, Date = DateOnly.ParseExact("2020-01-01", "yyyy-mm-dd") },
-        new(){ SupplyId = 2, Quantity = 2, Date = DateOnly.ParseExact("2020-01-01", "yyyy-mm-dd") },
-        new(){ SupplyId = 3, Quantity = 3, Date = DateOnly.ParseExact("2020-01-01", "yyyy-mm-dd") },
-        new(){ SupplyId = 4, Quantity = 4, Date = DateOnly.ParseExact("2020-01-01", "yyyy-mm-dd") },
-        new(){ SupplyId = 5, Quantity = 5, Date = DateOnly.ParseExact("2021-01-01", "yyyy-mm-dd") },
-        new(){ SupplyId = 6, Quantity = 6, Date = DateOnly.ParseExact("2021-01-01", "yyyy-mm-dd") },
-        new(){ SupplyId = 7, Quantity = 7, Date = DateOnly.ParseExact("2021-01-01", "yyyy-mm-dd") },
-        new(){ SupplyId = 8, Quantity = 8, Date = DateOnly.ParseExact("2022-01-01", "yyyy-mm-dd") },
-        new(){ SupplyId = 9, Quantity = 9, Date = DateOnly.ParseExact("2022-01-01", "yyyy-mm-dd") },
-        new(){ SupplyId = 10, Quantity = 10, Date = DateOnly.ParseExact("2022-01-01", "yyyy-mm-dd") },
+        new()
+        {
+            EnterpriseRegistrationNumber = 0, SupplierId = 0, Quantity = 1,
+            Date = DateOnly.ParseExact("2020-01-01", "yyyy-mm-dd")
+        },
+        new()
+        {
+            EnterpriseRegistrationNumber = 0, SupplierId = 4, Quantity = 1,
+            Date = DateOnly.ParseExact("2020-01-01", "yyyy-mm-dd")
+        },
+        new()
+        {
+            EnterpriseRegistrationNumber = 1, SupplierId = 0, Quantity = 1,
+            Date = DateOnly.ParseExact("2020-01-01", "yyyy-mm-dd")
+        },
+        new()
+        {
+            EnterpriseRegistrationNumber = 1, SupplierId = 1, Quantity = 1,
+            Date = DateOnly.ParseExact("2020-01-01", "yyyy-mm-dd")
+        },
+        new()
+        {
+            EnterpriseRegistrationNumber = 1, SupplierId = 2, Quantity = 1,
+            Date = DateOnly.ParseExact("2020-01-01", "yyyy-mm-dd")
+        },
+        new()
+        {
+            EnterpriseRegistrationNumber = 1, SupplierId = 3, Quantity = 1,
+            Date = DateOnly.ParseExact("2020-01-01", "yyyy-mm-dd")
+        },
+        new()
+        {
+            EnterpriseRegistrationNumber = 1, SupplierId = 4, Quantity = 1,
+            Date = DateOnly.ParseExact("2020-01-01", "yyyy-mm-dd")
+        },
+        new()
+        {
+            EnterpriseRegistrationNumber = 5, SupplierId = 1, Quantity = 1,
+            Date = DateOnly.ParseExact("2020-01-01", "yyyy-mm-dd")
+        },
+        new()
+        {
+            EnterpriseRegistrationNumber = 6, SupplierId = 1, Quantity = 1,
+            Date = DateOnly.ParseExact("2020-01-01", "yyyy-mm-dd")
+        },
+        new()
+        {
+            EnterpriseRegistrationNumber = 7, SupplierId = 2, Quantity = 1,
+            Date = DateOnly.ParseExact("2021-01-01", "yyyy-mm-dd")
+        },
+        new()
+        {
+            EnterpriseRegistrationNumber = 8, SupplierId = 3, Quantity = 1,
+            Date = DateOnly.ParseExact("2021-01-01", "yyyy-mm-dd")
+        },
+        new()
+        {
+            EnterpriseRegistrationNumber = 9, SupplierId = 4, Quantity = 1,
+            Date = DateOnly.ParseExact("2022-01-01", "yyyy-mm-dd")
+        },
     ];
 
     public List<Enterprise> Enterprises =
     [
         new()
         {
-            RegistrationNumber = 1,
-            Type = EnterpriseType.Agriculture,
+            EnterpriseTypeId = 0,
             Name = "Enterprise 1",
             Address = "Address 1",
-            PhoneNumber = "PhoneNumber 1",
-            Form = FormOfOwnership.Private,
+            PhoneNumber = "PhoneNum 1",
+            FormId = 3,
             EmployeesNumber = 1,
             TotalArea = 1
         },
         new()
         {
-            RegistrationNumber = 2,
-            Type = EnterpriseType.Agriculture,
+            EnterpriseTypeId = 0,
             Name = "Enterprise 2",
             Address = "Address 2",
-            PhoneNumber = "PhoneNumber 2",
-            Form = FormOfOwnership.JointStock,
+            PhoneNumber = "PhoneNum 2",
+            FormId = 4,
             EmployeesNumber = 2,
             TotalArea = 2
         },
         new()
         {
-            RegistrationNumber = 3,
-            Type = EnterpriseType.HeavyIndustry,
+            EnterpriseTypeId = 3,
             Name = "Enterprise 3",
             Address = "Address 3",
-            PhoneNumber = "PhoneNumber 3",
-            Form = FormOfOwnership.MunicipalUrban,
+            PhoneNumber = "PhoneNum 3",
+            FormId = 1,
             EmployeesNumber = 3,
             TotalArea = 3
         },
         new()
         {
-            RegistrationNumber = 4,
-            Type = EnterpriseType.HeavyIndustry,
+            EnterpriseTypeId = 3,
             Name = "Enterprise 4",
             Address = "Address 4",
-            PhoneNumber = "PhoneNumber 4",
-            Form = FormOfOwnership.StateFederal,
+            PhoneNumber = "PhoneNum 4",
+            FormId = 0,
             EmployeesNumber = 4,
             TotalArea = 4
         },
         new()
         {
-            RegistrationNumber = 5,
-            Type = EnterpriseType.MaterialAndTechnicalSupply,
+            EnterpriseTypeId = 5,
             Name = "Enterprise 5",
             Address = "Address 5",
-            PhoneNumber = "PhoneNumber 5",
-            Form = FormOfOwnership.TOO,
+            PhoneNumber = "PhoneNum 5",
+            FormId = 2,
             EmployeesNumber = 5,
             TotalArea = 5
         },
         new()
         {
-            RegistrationNumber = 6,
-            Type = EnterpriseType.MaterialAndTechnicalSupply,
+            EnterpriseTypeId = 5,
             Name = "Enterprise 6",
             Address = "Address 6",
-            PhoneNumber = "PhoneNumber 6",
-            Form = FormOfOwnership.Private,
+            PhoneNumber = "PhoneNum 6",
+            FormId = 3,
             EmployeesNumber = 6,
             TotalArea = 6
         },
         new()
         {
-            RegistrationNumber = 7,
-            Type = EnterpriseType.LightIndustry,
+            EnterpriseTypeId = 2,
             Name = "Enterprise 7",
             Address = "Address 7",
-            PhoneNumber = "PhoneNumber 7",
-            Form = FormOfOwnership.TOO,
+            PhoneNumber = "PhoneNum 7",
+            FormId = 2,
             EmployeesNumber = 7,
             TotalArea = 7
         },
         new()
         {
-            RegistrationNumber = 8,
-            Type = EnterpriseType.LightIndustry,
+            EnterpriseTypeId = 2,
             Name = "Enterprise 8",
             Address = "Address 8",
-            PhoneNumber = "PhoneNumber 8",
-            Form = FormOfOwnership.MunicipalUrban,
+            PhoneNumber = "PhoneNum 8",
+            FormId = 1,
             EmployeesNumber = 8,
             TotalArea = 8
         },
         new()
         {
-            RegistrationNumber = 9,
-            Type = EnterpriseType.Building,
+            EnterpriseTypeId = 4,
             Name = "Enterprise 9",
             Address = "Address 9",
-            PhoneNumber = "PhoneNumber 9",
-            Form = FormOfOwnership.MunicipalUrban,
+            PhoneNumber = "PhoneNum 9",
+            FormId = 1,
             EmployeesNumber = 9,
             TotalArea = 9
         },
         new()
         {
-            RegistrationNumber = 10,
-            Type = EnterpriseType.Transport,
+            EnterpriseTypeId = 2,
             Name = "Enterprise 10",
             Address = "Address 10",
-            PhoneNumber = "PhoneNumber 10",
-            Form = FormOfOwnership.TOO,
+            PhoneNumber = "PhoneNum 10",
+            FormId = 2,
             EmployeesNumber = 10,
             TotalArea = 10
         }
     ];
 
-    public List<Supplier> Suppliers;
+    public List<Supplier> Suppliers =
+    [
+        new() { Name = "Supplier 0" },
+        new() { Name = "Supplier 1" },
+        new() { Name = "Supplier 2" },
+        new() { Name = "Supplier 3" },
+        new() { Name = "Supplier 4" }
+    ];
+
 
     public DataProvider()
     {
-        Suppliers =
-        [
-            new Supplier
-            {
-                Id = 1,
-                Name = "Supplier 1",
-                Supplies = Supplies.Slice(1, 2),
-                Enterprises = [Enterprises[0], Enterprises[1]]
-            },
-            new Supplier
-            {
-                Id = 2,
-                Name = "Supplier 2",
-                Supplies = Supplies.Slice(3, 1),
-                Enterprises = [Enterprises[5], Enterprises[6], Enterprises[1]]
-            },
-            new Supplier
-            {
-                Id = 3,
-                Name = "Supplier 3",
-                Supplies = Supplies.Slice(6, 3),
-                Enterprises = [Enterprises[1], Enterprises[7], Enterprises[1]]
-            },
-            new Supplier
-            {
-                Id = 4,
-                Name = "Supplier 4",
-                Supplies = Supplies.Slice(1, 5),
-                Enterprises = [Enterprises[8], Enterprises[1]]
-            },
-            new Supplier
-            {
-                Id = 5,
-                Name = "Supplier 5",
-                Supplies = Supplies.Slice(8, 1),
-                Enterprises = [Enterprises[0], Enterprises[9], Enterprises[1]]
-            }
-        ];
-        Enterprises[0].Suppliers = [Suppliers[0], Suppliers[4]];
-        Enterprises[1].Suppliers = [Suppliers[0], Suppliers[1], Suppliers[2], Suppliers[3], Suppliers[4]];
-        Enterprises[5].Suppliers = [Suppliers[1]];
-        Enterprises[6].Suppliers = [Suppliers[1]];
-        Enterprises[7].Suppliers = [Suppliers[2]];
-        Enterprises[8].Suppliers = [Suppliers[3]];
-        Enterprises[9].Suppliers = [Suppliers[4]];
+        Supplies[0].Supplier = Suppliers[0];
+        Supplies[0].Enterprise = Enterprises[0];
+        Supplies[1].Supplier = Suppliers[4];
+        Supplies[1].Enterprise = Enterprises[0];
+        Supplies[2].Supplier = Suppliers[0];
+        Supplies[2].Enterprise = Enterprises[1];
+        Supplies[3].Supplier = Suppliers[1];
+        Supplies[3].Enterprise = Enterprises[1];
+        Supplies[4].Supplier = Suppliers[2];
+        Supplies[4].Enterprise = Enterprises[1];
+        Supplies[5].Supplier = Suppliers[3];
+        Supplies[5].Enterprise = Enterprises[1];
+        Supplies[6].Supplier = Suppliers[4];
+        Supplies[6].Enterprise = Enterprises[1];
+        Supplies[7].Supplier = Suppliers[1];
+        Supplies[7].Enterprise = Enterprises[5];
+        Supplies[8].Supplier = Suppliers[1];
+        Supplies[8].Enterprise = Enterprises[6];
+        Supplies[9].Supplier = Suppliers[2];
+        Supplies[9].Enterprise = Enterprises[7];
+        Supplies[10].Supplier = Suppliers[3];
+        Supplies[10].Enterprise = Enterprises[8];
+        Supplies[11].Supplier = Suppliers[4];
+        Supplies[11].Enterprise = Enterprises[9];
+
+        Enterprises[0].Type = EnterpriseTypes[0];
+        Enterprises[0].Form = FormsOfOwnership[3];
+        Enterprises[0].Supplies = [Supplies[0], Supplies[1]];
+        Enterprises[1].Type = EnterpriseTypes[0];
+        Enterprises[1].Form = FormsOfOwnership[4];
+        Enterprises[1].Supplies = [Supplies[2], Supplies[3], Supplies[4], Supplies[5],  Supplies[6]];
+        Enterprises[2].Type = EnterpriseTypes[3];
+        Enterprises[2].Form = FormsOfOwnership[1];
+        Enterprises[3].Type = EnterpriseTypes[3];
+        Enterprises[3].Form = FormsOfOwnership[0];
+        Enterprises[4].Type = EnterpriseTypes[5];
+        Enterprises[4].Form = FormsOfOwnership[2];
+        Enterprises[5].Type = EnterpriseTypes[5];
+        Enterprises[5].Form = FormsOfOwnership[3];
+        Enterprises[5].Supplies = [Supplies[7]];
+        Enterprises[6].Type = EnterpriseTypes[2];
+        Enterprises[6].Form = FormsOfOwnership[2];
+        Enterprises[6].Supplies = [Supplies[8]];
+        Enterprises[7].Type = EnterpriseTypes[2];
+        Enterprises[7].Form = FormsOfOwnership[1];
+        Enterprises[7].Supplies = [Supplies[9]];
+        Enterprises[8].Type = EnterpriseTypes[4];
+        Enterprises[8].Form = FormsOfOwnership[1];
+        Enterprises[8].Supplies = [Supplies[10]];
+        Enterprises[9].Type = EnterpriseTypes[2];
+        Enterprises[9].Form = FormsOfOwnership[2];
+        Enterprises[9].Supplies = [Supplies[11]];
+        
+        Suppliers[0].Supplies = [Supplies[0], Supplies[2]];
+        Suppliers[1].Supplies = [Supplies[3], Supplies[7], Supplies[8]];
+        Suppliers[2].Supplies = [Supplies[4], Supplies[9]];
+        Suppliers[3].Supplies = [Supplies[5], Supplies[10]];
+        Suppliers[4].Supplies = [Supplies[1], Supplies[6], Supplies[11]];
     }
 }
--- DistrictEnterpriseStatisticalData.Domain.Test/DistrictEnterpriseStatisticalDataTest.cs
+++ DistrictEnterpriseStatisticalData.Domain.Test/DistrictEnterpriseStatisticalDataTest.cs
@@ -1,22 +1,30 @@
-﻿namespace DistrictEnterpriseStatisticalData.Domain.Test;
+﻿using DistrictEnterpriseStatisticalData.Domain.Repository;
 
-public class DistrictEnterpriseStatisticalDataTest(DataProvider dataProvider) : IClassFixture<DataProvider>
+namespace DistrictEnterpriseStatisticalData.Domain.Test;
+
+public class DistrictEnterpriseStatisticalDataTest(TestDB testDb) : IClassFixture<TestDB>
 {
-    private readonly DataProvider _dataProvider = dataProvider;
+    
+    private readonly EnterpriseRepository _enterpriseRepository = testDb.EnterpriseRepository;
+    
+    private readonly SupplierRepository _supplierRepository = testDb.SupplierRepository;
 
     [Fact]
     public void ReturnAllEnterpriseData()
     {
-        var enterprise = _dataProvider.Enterprises.First();
+        var enterprise = _enterpriseRepository.GetById(1);
+        Assert.NotNull(enterprise);
         Assert.Equal(1, enterprise.RegistrationNumber);
-        Assert.Equal(EnterpriseType.Agriculture, enterprise.Type);
+        Assert.Equal(1, enterprise.EnterpriseTypeId);
+        Assert.Equal(testDb.DataProvider.EnterpriseTypes[0], enterprise.Type);
         Assert.Equal("Enterprise 1", enterprise.Name);
         Assert.Equal("Address 1", enterprise.Address);
-        Assert.Equal("PhoneNumber 1", enterprise.PhoneNumber);
-        Assert.Equal(FormOfOwnership.Private, enterprise.Form);
+        Assert.Equal("PhoneNum 1", enterprise.PhoneNumber);
+        Assert.Equal(1, enterprise.FormId);
+        Assert.Equal(testDb.DataProvider.FormsOfOwnership[3], enterprise.Form);
         Assert.Equal(1, enterprise.EmployeesNumber);
         Assert.Equal(1, enterprise.TotalArea);
-        Assert.Equal([_dataProvider.Suppliers[0], _dataProvider.Suppliers[4]], enterprise.Suppliers);
+        Assert.Equal([testDb.DataProvider.Supplies[0], testDb.DataProvider.Supplies[1]], enterprise.Supplies);
     }
 
     [Fact]
@@ -25,72 +33,58 @@ public class DistrictEnterpriseStatisticalDataTest(DataProvider dataProvider) : 
         var date1 = new DateOnly(2020, 01, 02);
         var date2 = new DateOnly(2021, 01, 02);
 
-        var suppliers = _dataProvider.Suppliers
-            .Where(supplier => supplier.Supplies.Any(supply => supply.Date >= date1 && supply.Date <= date2))
-            .OrderBy(s => s.Name).ToList();
-        Assert.Equal([_dataProvider.Suppliers[2], _dataProvider.Suppliers[3]], suppliers);
+        var suppliers = _supplierRepository.ReturnSupplyBetweenDates(date1, date2);
+        Assert.Equal([testDb.DataProvider.Suppliers[2], testDb.DataProvider.Suppliers[3]], suppliers);
     }
 
     [Fact]
     public void ReturnEnterprisesCountForEachSupplier()
     {
-        var suppliers = _dataProvider.Suppliers;
+        var suppliers = _supplierRepository.ReturnEnterprisesCountForEachSupplier().ToList();
         
-        Assert.Equal(2, suppliers[0].Enterprises.Count);
-        Assert.Equal(3, suppliers[1].Enterprises.Count);
-        Assert.Equal(3, suppliers[2].Enterprises.Count);
-        Assert.Equal(2, suppliers[3].Enterprises.Count);
-        Assert.Equal(3, suppliers[4].Enterprises.Count);
+        Assert.Equal(2, suppliers[0].enterprisesCount);
+        Assert.Equal(3, suppliers[1].enterprisesCount);
+        Assert.Equal(2, suppliers[2].enterprisesCount);
+        Assert.Equal(3, suppliers[3].enterprisesCount);
+        Assert.Equal(2, suppliers[4].enterprisesCount);
     }
 
     [Fact]
     public void ReturnSuppliersCountForTypeAndForm()
     {
-        var groupedData = _dataProvider.Enterprises.SelectMany(enterprise => enterprise.Suppliers,
-                (enterprise, supplier) => new { enterprise.Type, enterprise.Form, supplier })
-            .GroupBy(e => new { e.Type, e.Form })
-            .Select(group => new
-            {
-                IndustryType = group.Key.Type,
-                OwnershipForm = group.Key.Form,
-                SupplierCount = group.Select(e => e.supplier).Distinct().Count()
-            })
-            .ToList();
-        Assert.True(groupedData[0].IndustryType == EnterpriseType.Agriculture &&
-                    groupedData[0].OwnershipForm == FormOfOwnership.Private &&
-                    groupedData[0].SupplierCount == 2);
-        Assert.True(groupedData[1].IndustryType == EnterpriseType.Agriculture &&
-                    groupedData[1].OwnershipForm == FormOfOwnership.JointStock &&
-                    groupedData[1].SupplierCount == 5);
-        Assert.True(groupedData[2].IndustryType == EnterpriseType.MaterialAndTechnicalSupply &&
-                    groupedData[2].OwnershipForm == FormOfOwnership.Private &&
-                    groupedData[2].SupplierCount == 1);
-        Assert.True(groupedData[3].IndustryType == EnterpriseType.LightIndustry &&
-                    groupedData[3].OwnershipForm == FormOfOwnership.TOO &&
-                    groupedData[3].SupplierCount == 1);
-        Assert.True(groupedData[4].IndustryType == EnterpriseType.LightIndustry &&
-                    groupedData[4].OwnershipForm == FormOfOwnership.MunicipalUrban &&
-                    groupedData[4].SupplierCount == 1);
-        Assert.True(groupedData[5].IndustryType == EnterpriseType.Building &&
-                    groupedData[5].OwnershipForm == FormOfOwnership.MunicipalUrban &&
-                    groupedData[5].SupplierCount == 1);
-        Assert.True(groupedData[6].IndustryType == EnterpriseType.Transport &&
-                    groupedData[6].OwnershipForm == FormOfOwnership.TOO &&
-                    groupedData[6].SupplierCount == 1);
+        var groupedData = _supplierRepository.ReturnSuppliersCountForTypeAndForm().ToList();
+        Assert.True(groupedData[0].type == testDb.DataProvider.EnterpriseTypes[0] &&
+                    groupedData[0].form == testDb.DataProvider.FormsOfOwnership[3] &&
+                    groupedData[0].supplierCount == 2);
+        Assert.True(groupedData[1].type == testDb.DataProvider.EnterpriseTypes[0] &&
+                    groupedData[1].form == testDb.DataProvider.FormsOfOwnership[4] &&
+                    groupedData[1].supplierCount == 5);
+        Assert.True(groupedData[2].type == testDb.DataProvider.EnterpriseTypes[5] &&
+                    groupedData[2].form == testDb.DataProvider.FormsOfOwnership[3] &&
+                    groupedData[2].supplierCount == 1);
+        Assert.True(groupedData[3].type == testDb.DataProvider.EnterpriseTypes[2] &&
+                    groupedData[3].form == testDb.DataProvider.FormsOfOwnership[2] &&
+                    groupedData[3].supplierCount == 2);
+        Assert.True(groupedData[4].type == testDb.DataProvider.EnterpriseTypes[2] &&
+                    groupedData[4].form == testDb.DataProvider.FormsOfOwnership[1] &&
+                    groupedData[4].supplierCount == 1);
+        Assert.True(groupedData[5].type == testDb.DataProvider.EnterpriseTypes[4] &&
+                    groupedData[5].form == testDb.DataProvider.FormsOfOwnership[1] &&
+                    groupedData[5].supplierCount == 1);
+        Assert.True(groupedData[6].type == testDb.DataProvider.EnterpriseTypes[3] &&
+                    groupedData[6].form == testDb.DataProvider.FormsOfOwnership[1] &&
+                    groupedData[6].supplierCount == 0);
     }
 
     [Fact]
     public void FiveMostSuppliedEnterprises()
     {
-        var topEnterprises = _dataProvider.Enterprises
-            .OrderByDescending(e => e.Suppliers.SelectMany(s => s.Supplies).Count())
-            .Take(5)
-            .ToList();
-        Assert.Equal(_dataProvider.Enterprises[1], topEnterprises[0]);
-        Assert.Equal(_dataProvider.Enterprises[8], topEnterprises[1]);
-        Assert.Equal(_dataProvider.Enterprises[0], topEnterprises[2]);
-        Assert.Equal(_dataProvider.Enterprises[7], topEnterprises[3]);
-        Assert.Equal(_dataProvider.Enterprises[5], topEnterprises[4]);
+        var topEnterprises = _enterpriseRepository.FiveMostSuppliedEnterprises().ToList();
+        Assert.Equal(testDb.DataProvider.Enterprises[1], topEnterprises[0]);
+        Assert.Equal(testDb.DataProvider.Enterprises[0], topEnterprises[1]);
+        Assert.Equal(testDb.DataProvider.Enterprises[6], topEnterprises[2]);
+        Assert.Equal(testDb.DataProvider.Enterprises[7], topEnterprises[3]);
+        Assert.Equal(testDb.DataProvider.Enterprises[9], topEnterprises[4]);
     }
 
     [Fact]
@@ -99,16 +93,12 @@ public class DistrictEnterpriseStatisticalDataTest(DataProvider dataProvider) : 
         var date1 = new DateOnly(2021, 01, 02);
         var date2 = new DateOnly(2022, 01, 02);
 
-        var supplierWithMaxSupplies = _dataProvider.Suppliers
-            .OrderByDescending(s => s.Supplies
-                .Where(supply => supply.Date >= date1 && supply.Date <= date2)
-                .Sum(supply => supply.Quantity))
-            .ToList();
+        var supplierWithMaxSupplies = _supplierRepository.MaxProvidedSuppliers(date1, date2).ToList();
 
-        Assert.Equal(_dataProvider.Suppliers[2], supplierWithMaxSupplies[0]);
-        Assert.Equal(_dataProvider.Suppliers[4], supplierWithMaxSupplies[1]);
-        Assert.Equal(_dataProvider.Suppliers[0], supplierWithMaxSupplies[2]);
-        Assert.Equal(_dataProvider.Suppliers[1], supplierWithMaxSupplies[3]);
-        Assert.Equal(_dataProvider.Suppliers[3], supplierWithMaxSupplies[4]);
+        Assert.Equal(testDb.DataProvider.Suppliers[4], supplierWithMaxSupplies[0]);
+        Assert.Equal(testDb.DataProvider.Suppliers[0], supplierWithMaxSupplies[1]);
+        Assert.Equal(testDb.DataProvider.Suppliers[1], supplierWithMaxSupplies[2]);
+        Assert.Equal(testDb.DataProvider.Suppliers[2], supplierWithMaxSupplies[3]);
+        Assert.Equal(testDb.DataProvider.Suppliers[3], supplierWithMaxSupplies[4]);
     }
 }
